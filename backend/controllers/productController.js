23350d33923415738ee5eaa2cf21dc05ec99546b
a7ridi <a7ridi@gmail.com>
2022-09-11 23:23:18 +0530

    backend routes are made

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,127 +5,179 @@ const ApiFeatures = require("../utils/apifeatures");
 
 // create product - Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
-  req.body.user = req.user.id;
+	req.body.user = req.user.id;
 
-  const products = await Product.create(req.body);
+	const products = await Product.create(req.body);
 
-  res.status(201).json({
-    success: true,
-    products,
-  });
+	res.status(201).json({
+		success: true,
+		products,
+	});
 });
 
 // get products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const resultPerPage = 5;
-  const apiFeatures = new ApiFeatures(Product.find(), req.query)
-    .search()
-    .filter()
-    .pagination(resultPerPage);
-  // const product = await Product.find();
-  const product = await apiFeatures.query;
+	const resultPerPage = 5;
+	const apiFeatures = new ApiFeatures(Product.find(), req.query)
+		.search()
+		.filter()
+		.pagination(resultPerPage);
+	// const product = await Product.find();
+	const product = await apiFeatures.query;
 
-  res.status(200).json({
-    success: true,
-    product,
-  });
+	res.status(200).json({
+		success: true,
+		product,
+	});
 });
 
 // get product details
 exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
-  let product = await Product.findById(req.params.id);
+	let product = await Product.findById(req.params.id);
 
-  // if (!product) {
-  //   return res.status(500).json({
-  //     success: false,
-  //     message: "Product not found",
-  //   });
-  // }
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
+	// if (!product) {
+	//   return res.status(500).json({
+	//     success: false,
+	//     message: "Product not found",
+	//   });
+	// }
+	if (!product) {
+		return next(new ErrorHandler("Product not found", 404));
+	}
 
-  res.status(200).json({
-    success: true,
-    product,
-  });
+	res.status(200).json({
+		success: true,
+		product,
+	});
 });
 
 exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
-  let product = await Product.findById(req.params.id);
+	let product = await Product.findById(req.params.id);
 
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
+	if (!product) {
+		return next(new ErrorHandler("Product not found", 404));
+	}
 
-  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: true,
-  });
+	product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+		useFindAndModify: true,
+	});
 
-  res.status(200).json({
-    success: true,
-    product,
-  });
+	res.status(200).json({
+		success: true,
+		product,
+	});
 });
 
 // delete product
 exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
-  let product = await Product.findById(req.params.id);
+	let product = await Product.findById(req.params.id);
 
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
+	if (!product) {
+		return next(new ErrorHandler("Product not found", 404));
+	}
 
-  await product.remove();
+	await product.remove();
 
-  res.status(200).json({
-    success: true,
-    message: "Product deleted successfully",
-  });
+	res.status(200).json({
+		success: true,
+		message: "Product deleted successfully",
+	});
 });
 
 // create or update review
 exports.createUpdateReview = catchAsyncErrors(async (req, res, next) => {
-  const { rating, comment, productId } = req.body;
+	const { rating, comment, productId } = req.body;
 
-  const review = {
-    user: req.user._id,
-    name: req.user.name,
-    rating: Number(rating),
-    comment,
-  };
+	const review = {
+		user: req.user._id,
+		name: req.user.name,
+		rating: Number(rating),
+		comment,
+	};
 
-  const product = await Product.findById(productId);
+	const product = await Product.findById(productId);
 
-  const isReviewed = product.reviews.find(
-    (rev) => rev.user.toString() === req.user._id.toString()
-  );
+	const isReviewed = product.reviews.find(
+		(rev) => rev.user.toString() === req.user._id.toString()
+	);
 
-  if (isReviewed) {
-    product.reviews.forEach((rev) => {
-      if (rev.user.toString() === req.user._id.toString()) {
-        rev.rating = rating;
-        rev.comment = comment;
-      }
-    });
-  } else {
-    product.reviews.push(review);
-    product.numOReviews = product.reviews.length;
-  }
+	if (isReviewed) {
+		product.reviews.forEach((rev) => {
+			if (rev.user.toString() === req.user._id.toString()) {
+				rev.rating = rating;
+				rev.comment = comment;
+			}
+		});
+	} else {
+		product.reviews.push(review);
+		product.numOfReviews = product.reviews.length;
+	}
 
-  let avg = 0;
+	let avg = 0;
 
-  product.reviews.forEach((rev) => {
-    avg += rev.rating;
-  });
+	product.reviews.forEach((rev) => {
+		avg += rev.rating;
+	});
 
-  product.ratings = avg / product.reviews.length;
+	product.ratings = avg / product.reviews.length;
 
-  await product.save({ validateBeforeSave: false });
+	await product.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    success: true,
-  });
+	res.status(200).json({
+		success: true,
+	});
+});
+
+// get product review
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+	const product = await Product.findById(req.query.id);
+
+	if (!product) {
+		return next(new ErrorHandler("Product not found", 404));
+	}
+
+	res.status(200).json({
+		success: true,
+		reviews: product.reviews,
+	});
+});
+
+// delete review
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+	const product = await Product.findById(req.query.productId);
+
+	if (!product) {
+		return next(new ErrorHandler("Product not found", 404));
+	}
+
+	const reviews = product.reviews.filter(
+		(rev) => rev._id.toString() !== req.query.id.toString()
+	);
+
+	let avg = 0;
+
+	reviews.forEach((rev) => {
+		avg += rev.rating;
+	});
+
+	const validateRatings = avg / reviews.length;
+	const ratings = isNaN(validateRatings) ? 0 : validateRatings;
+
+	const numOfReviews = reviews.length;
+
+	await Product.findByIdAndUpdate(
+		req.query.productId,
+		{ ratings: Number(ratings), reviews, numOfReviews },
+		{
+			new: true,
+			runValidators: true,
+			useFindAndModify: false,
+		}
+	);
+
+	res.status(200).json({
+		success: true,
+	});
 });
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,26 +1,38 @@
 const express = require("express");
 const {
-  getAllProducts,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-  getProductDetails,
-  createUpdateReview,
+	getAllProducts,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+	getProductDetails,
+	createUpdateReview,
+	getProductReviews,
+	deleteReview,
 } = require("../controllers/productController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+
 router
-  .route("/admin/product/new")
-  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+	.route("/admin/product/new")
+	.post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+
 router.route("/product/:id").get(getProductDetails);
+
 router
-  .route("/admin/product/:id")
-  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+	.route("/admin/product/:id")
+	.put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+
 router
-  .route("/admin/product/:id")
-  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+	.route("/admin/product/:id")
+	.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
 router.route("/review").put(isAuthenticatedUser, createUpdateReview);
+
+router
+	.route("/reviews")
+	.get(getProductReviews)
+	.delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
